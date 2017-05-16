a2c7ce7ebecec883e2436ca06649d30cc8135353
Eridho Buffery Rollian <en@Eridhos-iMac.home>
2017-05-16 23:00:47 +0200

    nyoba dulu

--- index1.java
+++ index1.java
@@ -3,7 +3,8 @@ public class test {
 	public static void main(String[] args){
 
 		int x = 1;
+		int z = 2;
 		int y = 15;
- 		System.out.println( "Hasil:  " + (x + y));
+ 		System.out.println( "Hasil:  " + (x + y - z));
 	}
 }
