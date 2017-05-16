3c8a8baedb02217bc3b56320581d5517687f1812
Annisa Yustina <i.charitable@yahoo.com>
2017-05-16 23:19:13 +0200

    Pake punya ridho

--- index1.java
+++ index1.java
@@ -2,16 +2,10 @@ public class test {
 
 	public static void main(String[] args){
 
-<<<<<<< HEAD
-		int x = 10;
-		int y = 14;
- 		System.out.println( "Jadinya:  " + (x + y));
-=======
 		int x = 1;
 
 		int z = 2;
 		int y = 15;
  		System.out.println( "Hasil:  " + (x + y - z));
->>>>>>> dbb7250997b0ce4abbadc943f967f634cb980758
 	}
 }
